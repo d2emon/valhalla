4bc2f4a5ccd0887d1f8978f24c3b3f03d6abfe57
Dmitry Kutsenko <d2emonium@gmail.com>
2017-06-21 16:06:39 +0300

    Blueprints

--- app.py
+++ app.py
@@ -37,7 +37,6 @@ def create_app(config_name='production'):
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
 
-
     # Setting up templates and static
     # app.template_folder = app.config.get('TEMPLATE_FOLDER', '')
     # app.static_folder = app.config.get('STATIC_FOLDER', '')
@@ -59,11 +58,11 @@ def create_app(config_name='production'):
     # from app import models
 
     # Installing blueprints
-    # from blueprints.admin import admin as admin_blueprint
-    # app.register_blueprint(admin_blueprint, url_prefix='/admin')
+    from blueprints.admin import admin as admin_blueprint
+    app.register_blueprint(admin_blueprint, url_prefix='/admin')
 
-    # from blueprints.home import home as home_blueprint
-    # app.register_blueprint(home_blueprint)
+    from blueprints.home import home as home_blueprint
+    app.register_blueprint(home_blueprint)
 
     # Customizing errors
     @app.errorhandler(403)
--- blueprints/home/__init__.py
+++ blueprints/home/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 
 
-admin = Blueprint('admin', __name__)
+home = Blueprint('home', __name__)
 
 
 from . import views
